73c583c73ac934161c528d2c1eeba822fc36b346
GeoLopez33 <105619628+GeoLopez33@users.noreply.github.com>
2023-04-03 17:54:04 -0400

    Create WordC.java

---
+++ WordC.java
@@ -0,0 +1,48 @@
+package application;
+
+public class WordC {
+	
+
+	String word;
+	int count;
+	
+	public WordC(String word) {
+		this.word=word;
+		count=1;
+	}
+	
+	public WordC(String word, int num) {
+		this.word=word;
+		count=num;
+	}
+	
+	public void getCountS() {
+		
+		System.out.println(word + ": " + count);
+	}
+	
+	public String getCountSS() {
+		
+		return (word + ": " + count);
+	}
+	
+	
+	public int getCount() {
+		
+		return count;
+	}
+	
+	public void upCount() {
+		count++;
+	}
+	
+	@Override
+	public String toString() {
+
+		return word;
+	}
+	
+	public void setZero() {
+		count=0;
+	}
+}
